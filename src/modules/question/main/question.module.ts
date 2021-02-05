bab27beeb4a0033c148b10de3239eabe71f83eda
Daniel Otaviano <danielpontes0@icloud.com>
2021-02-05 07:44:41 -0300

    refactor: remove some unsed imports

--- src/modules/question/main/question.module.ts
+++ src/modules/question/main/question.module.ts
@@ -3,12 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OptionRepository } from 'src/repositories/option.repository';
 import { QuestionRepository } from 'src/repositories/question.repository';
 import { Option } from '../../option/entity/option.entity';
-import { OptionModule } from '../../option/option.module';
 import { Question } from '../entity/question.entity';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 import { OptionsValidate } from '../validation/options-validate';
-import { ExamModule } from 'src/modules/exam/main/exam.module';
 import { ExamRepository } from 'src/repositories/exam.repository';
 import { Exam } from 'src/modules/exam/entity/exam.entity';
 

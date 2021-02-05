e9ee089bcc0e63bb6ed83bac2fc2043044b44b6a
Daniel Otaviano <danielpontes0@icloud.com>
2021-02-04 21:20:50 -0300

    test: ensure ExamController create call create method in service with correct values

---
+++ src/modules/question/main/question.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateQuestionDto } from '../dtos/create-question.dto';
+import { DeleteQuestionDto } from '../dtos/delete-question.dto';
+import { FindOneQuestionDto } from '../dtos/find-one-question.dto';
+import { ListQuestionDto } from '../dtos/list-question.dto';
+import {
+  UpdateQuestionDto,
+  UpdateQuestionIdDto,
+} from '../dtos/update-question.dto';
+import { Question } from '../entity/question.entity';
+import { QuestionServiceInterface } from '../interface/question.service.interface';
+
+@Controller('questions')
+export class QuestionController {
+  constructor(
+    @Inject('QuestionServiceInterface')
+    private readonly questionService: QuestionServiceInterface,
+  ) {}
+
+  @Post()
+  public async create(
+    @Body() questionDto: CreateQuestionDto,
+  ): Promise<Question> {
+    return this.questionService.create(questionDto);
+  }
+
+  @Get()
+  public async list(@Body() questionDto: ListQuestionDto): Promise<Question[]> {
+    return await this.questionService.list(questionDto);
+  }
+
+  @Get(':id')
+  public async findOne(
+    @Param() questionDto: FindOneQuestionDto,
+  ): Promise<Question> {
+    return await this.questionService.findOne(questionDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  public async delete(@Param() questionDto: DeleteQuestionDto) {
+    await this.questionService.delete(questionDto);
+  }
+
+  @HttpCode(204)
+  @Put(':id')
+  public async update(
+    @Param() questionIdDto: UpdateQuestionIdDto,
+    @Body() questionDto: UpdateQuestionDto,
+  ) {
+    await this.questionService.update(questionIdDto.id, questionDto);
+  }
+}
